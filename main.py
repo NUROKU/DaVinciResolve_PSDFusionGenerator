61bf5a05133fa44c29d8b6ed5f533a375c95fd46
NUROKU <shimau6@yahoo.co.jp>
2022-09-08 01:30:42 +0900

    英訳はした

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def __main__():
     root = tk.Tk()
     root.withdraw()
     psd_file_path = filedialog.askopenfilename(
-        title="DaVinci Resolveで使用したいPSDファイルを選択してください",
+        title="Select PSD File for create Fusion",
         filetypes=[("PSD", ".psd")],
     )
     output_folder_path = os.path.dirname(psd_file_path)
     generator_name = os.path.splitext(os.path.basename(psd_file_path))[0]
 
     try:
-        print("PSDFusionGeneratorを開始します。")
-        print("PSDを解析中です。（これには数十秒かかる場合があります）")
+        print("Start PSDFusionGenerator")
+        print("Checking PSD #It might takes few min")
         psd_divider = PsdDivider()
         created_folder = psd_divider.execute(psd_file_path, output_folder_path)
-        print("settingファイルを作成中です")
+        print("Creating .setting")
         setting_creator = SettingCreator()
         setting_creator.execute(created_folder,generator_name)
-        messagebox.showinfo("報告", "DaVinciResolveに取り込めました。\nEditページ→エフェクト→ジェネレーター→Fusionジェネレーターにあります")
+        messagebox.showinfo("Info", "Create Successful \nCheck Generator in Effects Library")
     except Exception as e:
-        messagebox.showerror("エラー", "DavinciResolveに取り込めませんでした。\n" + traceback.format_exc())
+        messagebox.showerror("Error", "Sometihing error\n" + traceback.format_exc())
 
 __main__()
